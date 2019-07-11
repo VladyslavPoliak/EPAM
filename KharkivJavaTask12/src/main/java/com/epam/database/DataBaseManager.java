de444bd8a19007b88c9a7115be751c52e716d423
Vlad_ok75@ukr.net <Vladok31104817>
2019-07-11 23:39:59 +0300

    task12

---
+++ KharkivJavaTask12/src/main/java/com/epam/database/DataBaseManager.java
@@ -0,0 +1,6 @@
+package com.epam.database;
+
+public class DataBaseManager {
+
+
+}
--- KharkivJavaTask12/src/main/java/com/epam/listener/ApplicationListener.java
+++ KharkivJavaTask12/src/main/java/com/epam/listener/ApplicationListener.java
@@ -1,7 +1,5 @@
 package com.epam.listener;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-
 import com.epam.captcha.CaptchaHandler;
 import com.epam.container.CaptchaHandlerContainer;
 import com.epam.container.UserContainer;
@@ -14,33 +12,86 @@ import com.epam.service.UserService;
 import com.epam.service.impl.CaptchaServiceImpl;
 import com.epam.service.impl.UserServiceImpl;
 import com.epam.utils.Constants;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @WebListener
 public class ApplicationListener implements ServletContextListener {
+    private static final Logger LOGGER = Logger.getLogger(ApplicationListener.class);
+    private final Properties applicationProperties = new Properties();
+    private BasicDataSource dataSource;
+
+    private UserContainer container = new UserContainer();
+
+    private UserDao userDao;
+    private CaptchaDao captchaDao;
+    private UserService userService;
+    private CaptchaService captchaService;
+    private CaptchaHandler handler;
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-
-        UserContainer container = new UserContainer();
-
-        UserDao userDao = new UserDaoImpl(container.getUserList());
-        UserService userService = new UserServiceImpl(userDao);
-
-        CaptchaDao captchaDao = new CaptchaDaoImpl();
-        CaptchaService captchaService = new CaptchaServiceImpl(captchaDao);
-
-        sce.getServletContext().setAttribute(Constants.USER_SERVICE, userService);
-        sce.getServletContext().setAttribute(Constants.CAPTCHA_SERVICE, captchaService);
-
+        loadApplicationProperties();
+        initDao();
+        initServices();
         ServletContext context = sce.getServletContext();
 
         String handlerName = context.getInitParameter(Constants.CAPTCHA_HANDLER);
-        CaptchaHandler handler = new CaptchaHandlerContainer().getCaptchaHandler(handlerName);
+        handler = new CaptchaHandlerContainer().getCaptchaHandler(handlerName);
+        setAttributeInServletContext(context);
+
+        dataSource = initDataSource();
+
+    }
+
+    private BasicDataSource initDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDefaultAutoCommit(false);
+        dataSource.setRollbackOnReturn(true);
+        dataSource.setDriverClassName(getApplicationProperty("db.driver"));
+        dataSource.setUrl(getApplicationProperty("db.url"));
+        dataSource.setUsername(getApplicationProperty("db.username"));
+        dataSource.setPassword(getApplicationProperty("db.password"));
+        dataSource.setInitialSize(Integer.parseInt(getApplicationProperty("db.pool.initSize")));
+        dataSource.setMaxTotal(Integer.parseInt(getApplicationProperty("db.pool.maxSize")));
+        LOGGER.info("dataSource created");
+        return dataSource;
+    }
+
+    private String getApplicationProperty(String key) {
+        return applicationProperties.getProperty(key);
+    }
+
+    private void setAttributeInServletContext(ServletContext context) {
+        context.setAttribute(Constants.USER_SERVICE, userService);
+        context.setAttribute(Constants.CAPTCHA_SERVICE, captchaService);
         context.setAttribute(Constants.CAPTCHA_PRESERVER, handler);
+    }
+
+    private void initServices() {
+        userService = new UserServiceImpl(userDao);
+        captchaService = new CaptchaServiceImpl(captchaDao);
+    }
+
+    private void initDao() {
+        userDao = new UserDaoImpl(container.getUserList());
+        captchaDao = new CaptchaDaoImpl();
+    }
+
+    private void loadApplicationProperties() {
+        try (InputStream in = ApplicationListener.class.getClassLoader().
+                getResourceAsStream("application.properties")) {
+            applicationProperties.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
