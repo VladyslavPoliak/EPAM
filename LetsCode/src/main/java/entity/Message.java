08561254cc5830bbfb9629d299affaf702c860bd
Vladyslav Poliak <vladyslav_poliak@epam.com>
2019-08-08 13:03:04 +0300

    Spring

---
+++ LetsCode/src/main/java/entity/Message.java
@@ -0,0 +1,35 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    private String tags;
+
+}
---
+++ LetsCode/src/main/java/repository/MessageRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import entity.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepository extends CrudRepository<Message, Long> {
+
+}
