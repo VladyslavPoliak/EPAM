42e90f1fafcfaf282ffb323d917afe84091f54eb
Vladyslav Poliak <vladyslav_poliak@epam.com>
2019-08-14 17:48:55 +0300

    profile edit

--- LetsCode/src/main/java/project/controller/UserController.java
+++ LetsCode/src/main/java/project/controller/UserController.java
@@ -2,32 +2,31 @@ package project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import project.entity.Role;
 import project.entity.User;
-import project.repository.UserRepository;
+import project.service.UserService;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserService userService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public String userList(Model model) {
-        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping("{user}")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
@@ -35,28 +34,31 @@ public class UserController {
         return "userEdit";
     }
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping
     public String userSave(
             @RequestParam String username,
             @RequestParam Map<String, String> form,
             @RequestParam("userId") User user) {
-        user.setUsername(username);
 
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::getAuthority)
-                .collect(Collectors.toSet());
+        userService.saveUser(user, username, form);
 
-        user.getRoles().clear();
-
-        form.keySet().forEach(k -> {
-            if (roles.contains(k)) {
-                user.getRoles().add(Role.valueOf(k));
-            }
-        });
-
-        userRepository.save(user);
         return "redirect:/user";
     }
 
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
 
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(@AuthenticationPrincipal User user,
+                                @RequestParam String password,
+                                @RequestParam String email) {
+        userService.updateProfile(user,password,email);
+        return "redirect:/user/profile";
+    }
 }
--- LetsCode/src/main/java/project/service/UserService.java
+++ LetsCode/src/main/java/project/service/UserService.java
@@ -10,8 +10,8 @@ import project.entity.Role;
 import project.entity.User;
 import project.repository.UserRepository;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -32,6 +32,61 @@ public class UserService implements UserDetailsService {
         if (userFromDb != null) {
             return false;
         }
+        sendMessage(user);
+        userRepository.save(user);
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepository.findByActivationCode(code);
+        if (user == null) {
+            return false;
+        }
+        user.setActivationCode(null);
+        userRepository.save(user);
+        return true;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::getAuthority)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        form.keySet().forEach(k -> {
+            if (roles.contains(k)) {
+                user.getRoles().add(Role.valueOf(k));
+            }
+        });
+        userRepository.save(user);
+    }
+
+    public void updateProfile(User user, String password, String email) {
+        String userEmail = user.getEmail();
+        boolean isEmailChanged = (email != null && !email.equals(userEmail)) || (userEmail != null && !userEmail.equals(email));
+
+        if (isEmailChanged) {
+            user.setEmail(email);
+            user.setActivationCode(UUID.randomUUID().toString());
+        }
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userRepository.save(user);
+        if (isEmailChanged) {
+            sendMessage(user);
+        }
+    }
+
+    private void sendMessage(User user) {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
@@ -43,18 +98,5 @@ public class UserService implements UserDetailsService {
                     user.getUsername(), user.getActivationCode());
             mailSender.send(user.getEmail(), "Activation code", message);
         }
-
-        userRepository.save(user);
-        return true;
-    }
-
-    public boolean activateUser(String code) {
-        User user = userRepository.findByActivationCode(code);
-        if (user != null) {
-            return false;
-        }
-        user.setActivationCode(null);
-        userRepository.save(user);
-        return true;
     }
 }
